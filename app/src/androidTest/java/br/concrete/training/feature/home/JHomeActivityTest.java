17c43762e19f92f0e95d3efaa7415bfc4f7ad83c
lucas campos <lucasgcampos.contato@gmail.com>
2017-12-15 14:13:43 -0200

    kotlin challenge

---
+++ app/src/androidTest/java/br/concrete/training/feature/home/JHomeActivityTest.java
@@ -0,0 +1,80 @@
+package br.concrete.training.feature.home;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import br.concrete.training.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.replaceText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+/**
+ * @author Lucas Campos
+ */
+public class JHomeActivityTest {
+
+    private static final String DEFAULT_EMPTY_DESCRIPTION = "-";
+    private static final String DIALOG_TITLE = "Atenção";
+    private static final String TITLE = "título";
+    private static final String DESCRIPTION = "Alguma descrição relevante ao tópico";
+    private static final String EMPTY_STRING = "";
+
+    @Rule
+    public ActivityTestRule<JHomeActivity> activityRule = new ActivityTestRule<>(JHomeActivity.class, true, false);
+
+    @Test
+    public void shouldCreateItemCorrectly() {
+        // given
+        activityRule.launchActivity(new Intent());
+
+        // when
+        onView(withId(R.id.add_item)).perform(click());
+        onView(withId(R.id.task)).perform(replaceText(TITLE));
+        onView(withId(R.id.description)).perform(replaceText(DESCRIPTION));
+        onView(withId(R.id.add_item)).perform(click());
+
+        // then
+        onView(withText(TITLE)).check(matches(isDisplayed()));
+        onView(withText(DESCRIPTION)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldTryCreateItemWithEmptyTask() {
+        // given
+        activityRule.launchActivity(new Intent());
+
+        // when
+        onView(withId(R.id.add_item)).perform(click());
+        onView(withId(R.id.task)).perform(replaceText(EMPTY_STRING));
+        onView(withId(R.id.description)).perform(replaceText(DESCRIPTION));
+        onView(withId(R.id.add_item)).perform(click());
+
+        // then
+        onView(withText(DIALOG_TITLE)).check(matches(isDisplayed()));
+    }
+
+    @Test
+    public void shouldTryCreateItemWithEmptyDescription() {
+        // given
+        activityRule.launchActivity(new Intent());
+
+        // when
+        onView(withId(R.id.add_item)).perform(click());
+        onView(withId(R.id.task)).perform(replaceText(TITLE));
+        onView(withId(R.id.description)).perform(replaceText(EMPTY_STRING));
+        onView(withId(R.id.add_item)).perform(click());
+
+        // then
+        onView(withText(TITLE)).check(matches(isDisplayed()));
+        onView(withText(DEFAULT_EMPTY_DESCRIPTION)).check(matches(isDisplayed()));
+    }
+
+}
---
+++ app/src/main/java/br/concrete/training/feature/item/JItemActivity.java
@@ -0,0 +1,61 @@
+package br.concrete.training.feature.item;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+
+import br.concrete.training.data.model.Item;
+import br.concrete.training.R;
+
+import static br.concrete.training.feature.home.JHomeActivity.ITEM_EXTRAS;
+
+/**
+ * @author Lucas Campos
+ */
+public class JItemActivity extends AppCompatActivity {
+
+    private FloatingActionButton fab;
+    private TextInputEditText task;
+    private TextInputEditText description;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item);
+        setUpScreen();
+    }
+
+    private void setUpScreen() {
+        bindViews();
+
+        fab.setOnClickListener(view -> {
+            if (task.getText().toString().isEmpty()) {
+                new AlertDialog.Builder(this)
+                        .setTitle("Atenção")
+                        .setMessage("O campo 'tarefa' é obrigatório!")
+                        .setPositiveButton(android.R.string.ok, (dialog, i) -> dialog.dismiss())
+                        .show();
+            } else {
+                setResult(RESULT_OK, new Intent().putExtra(ITEM_EXTRAS, getFormItem()));
+                finish();
+            }
+        });
+    }
+
+    private void bindViews() {
+        fab = findViewById(R.id.add_item);
+        task = findViewById(R.id.task);
+        description = findViewById(R.id.description);
+    }
+
+    private Item getFormItem() {
+        return new Item(
+                task.getText().toString(),
+                description.getText().toString().isEmpty() ? "-" : description.getText().toString()
+        );
+    }
+}
